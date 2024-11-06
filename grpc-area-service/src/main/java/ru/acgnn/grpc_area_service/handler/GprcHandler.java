5690258f8d24838bdc439270fa2a2a42ce537a4e
Rustam Khudoyan <dreamang2toplay@yandex.ru>
2024-11-06 23:17:43 +0300

    feat: Добавлен маппер от HttpStatus -> gRPC Status

--- grpc-area-service/src/main/java/ru/acgnn/grpc_area_service/handler/GprcHandler.java
+++ grpc-area-service/src/main/java/ru/acgnn/grpc_area_service/handler/GprcHandler.java
@@ -3,15 +3,18 @@ package ru.acgnn.grpc_area_service.handler;
 import org.springframework.http.HttpStatus;
 
 import io.grpc.Status;
+import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.advice.GrpcAdvice;
 import net.devh.boot.grpc.server.advice.GrpcExceptionHandler;
 import ru.acgnn.grpc_area_service.exception.ApiServiceException;
 
+@Slf4j
 @GrpcAdvice
 public class GprcHandler {
 
     @GrpcExceptionHandler(ApiServiceException.class)
     public Status handleInvalidArgument(ApiServiceException e) {
+        log.debug("ApiServiceException: {}", e.getMessage());
         return getStatus(e.getStatus()).withDescription(e.getMessage());
     }
 
